558fff27075dc655997d9796811326cae219041f
diego249 <ballesterdiego24@gmail.com>
2021-07-02 18:03:48 -0300

    dsa

---
+++ tp_programado_78330/ejercicio59/clases.py
@@ -0,0 +1,317 @@
+import random
+
+class Encargado:
+    def __init__(self, id, estado, disponible, cola, cola_asig, terminal1, terminal2, terminal3):
+        self.id = id
+        self.estado = estado
+        self.max = disponible
+        self.disponible = disponible
+        self.cola = cola
+        self.cola_asig = cola_asig
+        self.terminal1 = terminal1
+        self.terminal2 = terminal2
+        self.terminal3 = terminal3
+        self.fin_cobro = ''
+        self.fin_asignacion = ''
+        self.tiempo_turno = ''
+        self.importe = ''
+
+    def aumentardisponible(self):
+        self.disponible += 1
+        if self.disponible == self.max:
+            self.estado = "Libre"
+        else:
+            self.estado = "Ocupado"
+
+    def disminuirdisponible(self):
+        self.disponible -= 1
+        if self.disponible == 0:
+            self.estado = "Ocupado"
+        else:
+            self.estado = "Libre"
+
+    def comenzar_asignacion(self):
+        if self.estado == "Libre" and (self.terminal1.estado == 'Libre' or self.terminal2.estado == 'Libre' or self.terminal3.estado == 'Libre'):
+            self.disminuirdisponible()
+            if self.cola_asig > 0:
+                self.cola_asig -= 1
+            return True
+        else:
+            self.cola_asig += 1
+            return False
+
+    def asignar_terminal(self):
+        if self.terminal1.estado == 'Libre':
+            self.aumentardisponible()
+            self.terminal1.disminuirdisponible()
+            return self.terminal1
+        elif self.terminal2.estado == 'Libre':
+            self.aumentardisponible()
+            self.terminal2.disminuirdisponible()
+            return self.terminal2
+        elif self.terminal3.estado == 'Libre':
+            self.aumentardisponible()
+            self.terminal3.disminuirdisponible()
+            return self.terminal3
+
+    def asignar_cobro(self):
+        if self.estado == 'Libre':
+            self.disminuirdisponible()
+            if self.cola > 0:
+                self.cola -= 1
+            return True
+        else:
+            self.cola += 1
+            return False
+
+
+class Terminal:
+    def __init__(self, id, estado, disponible):
+        self.id = id
+        self.estado = estado
+        self.max = disponible
+        self.disponible = disponible
+        self.tiempo_liberacion = ''
+
+    def aumentardisponible(self):
+        self.disponible += 1
+        if self.disponible == self.max:
+            self.estado = "Libre"
+        else:
+            self.estado = "Disponible"
+
+    def disminuirdisponible(self):
+        self.disponible -= 1
+        if self.disponible == 0:
+            self.estado = "Ocupado"
+        else:
+            self.estado = "Disponible"
+
+class Mozo:
+    def __init__(self, id, estado, disponible, cola):
+        self.id = id
+        self.estado = estado
+        self.max = disponible
+        self.disponible = disponible
+        self.cola = cola
+        self.fin_entrega = ''
+
+    def aumentardisponible(self):
+        self.disponible += 1
+        if self.disponible == self.max:
+            self.estado = "Libre"
+        else:
+            self.estado = "Ocupado"
+
+    def disminuirdisponible(self):
+        self.disponible -= 1
+        if self.disponible == 0:
+            self.estado = "Ocupado"
+        else:
+            self.estado = "Libre"
+
+class Impresora:
+    def __init__(self, id, estado, disponible, cola):
+        self.id = id
+        self.estado = estado
+        self.max = disponible
+        self.disponible = disponible
+        self.cola = cola
+        self.fin_impresion = ''
+
+    def aumentardisponible(self):
+        self.disponible += 1
+        if self.disponible == self.max:
+            self.estado = "Libre"
+        else:
+            self.estado = "Ocupado"
+
+    def disminuirdisponible(self):
+        self.disponible -= 1
+        if self.disponible == 0:
+            self.estado = "Ocupado"
+        else:
+            self.estado = "Libre"
+
+class Cliente:
+    def __init__(self, id, encargado, mozo, impresora):
+        self.id = id
+        self.estado = 'C'
+        self.tiempo_llegada = ''
+        self.gasto_individual = 0
+        self.terminal_asignada = ''
+        self.nro_terminal = 0
+        self.servidor_actual = ''
+        self.tiempo_asignacion = ''
+        self.tiempo_fin_turno = ''
+        self.tiempo_entrega_pedido = ''
+        self.tiempo_fin_impresion = ''
+        self.tiempo_fin_cobro = ''
+        self.en_cola = False
+        self.en_cola_asig = False
+        self.pedido = False
+        self.impresion = False
+        self.encargado = encargado
+        self.mozo = mozo
+        self.impresora = impresora
+        self.duracion_turno = ''
+
+    def realizar_pedido(self):
+        aleatorio = random.random()
+        if aleatorio < 0.5:
+            return True
+        else:
+            return False
+
+    def buscar_impresion(self):
+        aleatorio = random.random()
+        if aleatorio < 0.5:
+            return True
+        else:
+            return False
+
+    def definir_duracion(self):
+        aleatorio = random.random()
+        if aleatorio < 0.5:
+            return 60
+        else:
+            return 30
+
+    def ejecutar(self, reloj):
+
+        if self.estado == 'C' or self.en_cola_asig:
+
+            self.encargado.tiempo_turno = ''
+            self.encargado.importe = ''
+
+            if self.estado == 'C':
+                self.tiempo_llegada = reloj
+            asignado = self.encargado.comenzar_asignacion()
+            if asignado:
+                self.estado = 'SA'
+                self.en_cola_asig = False
+                self.tiempo_asignacion = reloj + 0.33
+                self.encargado.fin_asignacion = self.tiempo_asignacion
+                return
+            else:
+                self.estado = 'EA'
+                self.en_cola_asig = True
+                return
+
+        elif self.tiempo_asignacion == reloj and self.estado == 'SA':
+            self.duracion_turno = self.definir_duracion()
+            self.tiempo_fin_turno = self.tiempo_asignacion + self.duracion_turno
+            self.estado = 'ET'
+            self.terminal_asignada = self.encargado.asignar_terminal()
+            self.terminal_asignada.tiempo_liberacion = self.tiempo_fin_turno
+            self.nro_terminal = self.terminal_asignada.id
+            self.encargado.fin_asignacion = ''
+            self.tiempo_asignacion = ''
+
+            if self.duracion_turno == 60:
+                self.gasto_individual += 3.5
+            else:
+                self.gasto_individual += 2
+
+            self.encargado.tiempo_turno = self.duracion_turno
+            self.encargado.importe = self.gasto_individual
+
+            self.pedido = self.realizar_pedido()
+            if self.pedido and self.mozo.estado == 'Libre':
+                self.mozo.disminuirdisponible()
+                self.gasto_individual += 2
+                self.tiempo_entrega_pedido = reloj + 3
+                self.pedido = False
+                self.mozo.fin_entrega = self.tiempo_entrega_pedido
+                if self.mozo.cola > 0:
+                    self.mozo.cola -= 1
+            elif self.pedido and self.mozo.estado == 'Ocupado':
+                self.mozo.cola += 1
+            return
+
+        elif self.tiempo_entrega_pedido == reloj:
+            self.mozo.aumentardisponible()
+            self.tiempo_entrega_pedido = ''
+            self.mozo.fin_entrega = ''
+
+            self.encargado.tiempo_turno = ''
+            self.encargado.importe = ''
+            return
+
+        elif self.tiempo_fin_turno == reloj:
+            self.terminal_asignada.aumentardisponible()
+            self.terminal_asignada.tiempo_liberacion = ''
+            self.tiempo_fin_turno = ''
+            self.estado = 'FT'
+
+            self.impresion = self.buscar_impresion()
+            if self.impresion and self.impresora.estado == 'Libre':
+                self.impresora.disminuirdisponible()
+                self.gasto_individual += 0.5
+                self.tiempo_fin_impresion = reloj + 1
+                self.impresion = False
+                self.impresora.fin_impresion = self.tiempo_fin_impresion
+                self.estado = 'SI'
+                return
+
+            elif self.impresion and self.impresora.estado == 'Ocupado':
+                self.impresora.cola += 1
+                self.estado = 'EI'
+                return
+
+            elif not self.impresion:
+                paga = self.encargado.asignar_cobro()
+
+                if paga:
+                    self.tiempo_fin_cobro = reloj + 0.5
+                    self.estado = 'SC'
+                    self.en_cola = False
+                    return
+                else:
+                    self.estado = 'EC'
+                    self.en_cola = True
+                    return
+
+        elif self.tiempo_fin_impresion == reloj:
+            self.impresora.aumentardisponible()
+            self.impresora.fin_impresion = ''
+            self.tiempo_fin_impresion = ''
+            self.encargado.tiempo_turno = ''
+            self.encargado.importe = ''
+
+            paga = self.encargado.asignar_cobro()
+
+            if paga:
+                self.tiempo_fin_cobro = reloj + 0.5
+                self.estado = 'SC'
+                self.en_cola = False
+                return
+            else:
+                self.estado = 'EC'
+                self.en_cola = True
+                return
+
+        elif self.tiempo_fin_cobro == reloj:
+            self.encargado.aumentardisponible()
+            self.encargado.fin_cobro = ''
+            self.tiempo_fin_cobro = ''
+            self.estado = 'D'
+            return
+
+        elif self.en_cola:
+            paga = self.encargado.asignar_cobro()
+
+            if paga:
+                self.tiempo_fin_cobro = reloj + 0.5
+                self.estado = 'SC'
+                self.en_cola = False
+                return
+            else:
+                self.estado = 'EC'
+                self.en_cola = True
+                return
+
+
+
+
+
--- tp_programado_78330/ejercicio59/views.py
+++ tp_programado_78330/ejercicio59/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ParametersForm
 from django.views import generic
 import numpy as np
+from . import clases
 
 # Create your views here.
 def index(request):
@@ -19,42 +20,180 @@ class simulacion(generic.FormView):
 
         matriz = [[''] * 29 for f in range(1)]
         reloj = 0
+        proxima_llegada = 0
+        eventos = ["Inicialización", "Llegada Cliente", "Fin Asignacion", "Fin Entrega Pedido", "Fin Turno", "Fin Impresion", "Fin Cobro"]
+        evento = ''
+        init = True
+        llegada_cliente = False
+        asignacion_cliente = False
+        pedido_cliente = False
+        fin_turno_cliente = False
+        impresion_cliente = False
+        cobro_cliente = False
+        id_cliente = 0
+        clientes = []
+
+        #servidores
+        estados_encargado = ['Libre', 'Ocupado']
+        estados_terminales = ['Libre', 'Disponible', 'Ocupado']
+        estados_mozo = ['Libre', 'Ocupado']
+        estados_impresora = ['Libre', 'Ocupado']
+        terminal1 = clases.Terminal(1, estados_terminales[0], 1)
+        terminal2 = clases.Terminal(2, estados_terminales[0], 1)
+        terminal3 = clases.Terminal(3, estados_terminales[0], 1)
+        encargado = clases.Encargado(1, estados_encargado[0], 1, 0, 0, terminal1, terminal2, terminal3)
+        mozo = clases.Mozo(1, estados_mozo[0], 1, 0)
+        impresora = clases.Impresora(1, estados_impresora[0], 1, 0)
+        clientes_completados = 0
+        ac_costo = 0
+        promedio = 0
+
 
         while reloj <= fin:
+
+            if init:
+                evento = eventos[0]
+                proxima_llegada = reloj + 15
+                init = False
+
+            elif llegada_cliente:
+                evento = eventos[1]
+                proxima_llegada = reloj + 15
+                id_cliente += 1
+                clientes.append(clases.Cliente(id_cliente, encargado, mozo, impresora))
+
+                matriz_cliente = [[''] * 5 for f in range(len(matriz))]
+                matriz = np.hstack((matriz, matriz_cliente))
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+
+            elif asignacion_cliente:
+                evento = eventos[2]
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+                asignacion_cliente = False
+
+            elif pedido_cliente:
+                evento = eventos[3]
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+                pedido_cliente = False
+
+            elif fin_turno_cliente:
+                evento = eventos[4]
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+                fin_turno_cliente = False
+
+            elif impresion_cliente:
+                evento = eventos[5]
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+                impresion_cliente = False
+
+            elif cobro_cliente:
+                evento = eventos[6]
+                for cliente in clientes:
+                    cliente.ejecutar(reloj)
+                cobro_cliente = False
+
             if reloj <= relojFin and reloj >= relojInicio:
-                matriz[-1][0] = 0
-                matriz[-1][1] = 0
-                matriz[-1][2] = 0
-                matriz[-1][3] = 0
-                matriz[-1][4] = 0
-                matriz[-1][5] = 0
-                matriz[-1][6] = 0
-                matriz[-1][7] = 0
-                matriz[-1][8] = 0
-                matriz[-1][9] = 0
-                matriz[-1][10] = 0
-                matriz[-1][11] = 0
-                matriz[-1][12] = 0
-                matriz[-1][13] = 0
-                matriz[-1][14] = 0
-                matriz[-1][15] = 0
-                matriz[-1][16] = 0
-                matriz[-1][17] = 0
-                matriz[-1][18] = 0
+                matriz[-1][0] = evento
+                matriz[-1][1] = reloj
+                matriz[-1][2] = proxima_llegada
+                matriz[-1][3] = encargado.fin_asignacion
+                matriz[-1][4] = encargado.estado
+                matriz[-1][5] = encargado.cola
+                matriz[-1][6] = encargado.cola_asig
+                matriz[-1][7] = encargado.tiempo_turno
+                matriz[-1][8] = encargado.importe
+                matriz[-1][9] = terminal1.estado
+                matriz[-1][10] = terminal1.tiempo_liberacion
+                matriz[-1][11] = terminal2.estado
+                matriz[-1][12] = terminal2.tiempo_liberacion
+                matriz[-1][13] = terminal3.estado
+                matriz[-1][14] = terminal3.tiempo_liberacion
+                matriz[-1][15] = mozo.estado
+                matriz[-1][16] = mozo.fin_entrega
+                matriz[-1][17] = impresora.estado
+                matriz[-1][18] = impresora.fin_impresion
+
                 matriz[-1][19] = 0
                 matriz[-1][20] = 0
                 matriz[-1][21] = 0
                 matriz[-1][22] = 0
                 matriz[-1][23] = 0
-                matriz[-1][24] = 0
-                matriz[-1][25] = 0
-                matriz[-1][26] = 0
-                matriz[-1][27] = 0
-                matriz[-1][28] = 0
-                reloj += 1
+                for i in range(id_cliente):
+                    matriz[-1][24 + (i * 5)] = clientes[i].estado
+                    matriz[-1][25 + (i * 5)] = clientes[i].gasto_individual
+                    matriz[-1][26 + (i * 5)] = clientes[i].nro_terminal
+                    matriz[-1][27 + (i * 5)] = 'VACIO'
+                    matriz[-1][28 + (i * 5)] = 'VACIO'
+
                 vector_temporal = [''] * len(matriz[0])
                 matriz = np.vstack([matriz, vector_temporal])
 
+            # logico proximo evento
+            temporal_tiempo = proxima_llegada
+            llegada_cliente = True
 
-        return render(self.request, self.template_name, {"matrizResultado": matriz, "vectorEntrada": [fin, relojInicio, relojFin]
-        })
+            for cliente in clientes:
+
+                if cliente.tiempo_asignacion != '':
+                    if cliente.tiempo_asignacion < temporal_tiempo and cliente.estado == "SA":
+                        llegada_cliente = False
+                        asignacion_cliente = True
+                        pedido_cliente = False
+                        fin_turno_cliente = False
+                        impresion_cliente = False
+                        cobro_cliente = False
+                        temporal_tiempo = cliente.tiempo_asignacion
+                        break
+
+                if cliente.tiempo_fin_turno != '':
+                    if cliente.tiempo_fin_turno < temporal_tiempo and cliente.estado == "FT":
+                        llegada_cliente = False
+                        asignacion_cliente = False
+                        pedido_cliente = False
+                        fin_turno_cliente = True
+                        impresion_cliente = False
+                        cobro_cliente = False
+                        temporal_tiempo = cliente.tiempo_fin_turno
+                        break
+
+                if cliente.tiempo_entrega_pedido != '':
+                    if cliente.tiempo_entrega_pedido < temporal_tiempo and cliente.estado == "ET":
+                        llegada_cliente = False
+                        asignacion_cliente = False
+                        pedido_cliente = True
+                        fin_turno_cliente = False
+                        impresion_cliente = False
+                        cobro_cliente = False
+                        temporal_tiempo = cliente.tiempo_entrega_pedido
+                        break
+
+                if cliente.tiempo_fin_impresion != '':
+                    if cliente.tiempo_fin_impresion < temporal_tiempo and cliente.estado == "SI":
+                        llegada_cliente = False
+                        asignacion_cliente = False
+                        pedido_cliente = False
+                        fin_turno_cliente = False
+                        impresion_cliente = True
+                        cobro_cliente = False
+                        temporal_tiempo = cliente.tiempo_fin_impresion
+                        break
+
+                if cliente.tiempo_fin_cobro != '':
+                    if cliente.tiempo_fin_cobro < temporal_tiempo and cliente.estado == "SC":
+                        llegada_cliente = False
+                        asignacion_cliente = False
+                        pedido_cliente = False
+                        fin_turno_cliente = False
+                        impresion_cliente = False
+                        cobro_cliente = True
+                        temporal_tiempo = cliente.tiempo_fin_cobro
+                        break
+
+            reloj = temporal_tiempo
+
+        return render(self.request, self.template_name, {"matrizResultado": matriz, "vectorEntrada": [fin, relojInicio, relojFin]})
